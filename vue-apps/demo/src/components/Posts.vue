18d48b88ee8ffa4d207303b82ddfbc8928b10083
William Perry <durabilliam@gmail.com>
2021-08-09 15:23:39 -0230

    add demo api

--- vue-apps/demo/src/components/Posts.vue
+++ vue-apps/demo/src/components/Posts.vue
@@ -3,6 +3,7 @@
     <table class="table table-striped mt-4">
       <thead>
         <tr>
+          <th scope="col">UserId</th>
           <th scope="col">Id</th>
           <th scope="col">Title</th>
           <th scope="col">Body</th>
@@ -10,7 +11,7 @@
       </thead>
       <tbody>
         <tr v-for="post in posts" :key="post.id">
-          <th scope="row">1</th>
+          <td>{{ post.userId}}</td>
           <td>{{ post.id }}</td>
           <td>{{ post.title }}</td>
           <td>{{ post.body }}</td>
@@ -21,18 +22,28 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   name: "PostsExample",
   data() {
     return {
-      posts: [
-        { id: 1, title: 'Post 1', body: 'jiarfshviua'},
-        { id: 2, title: 'Post 2', body: 'jekrwhvei5u'},
-        { id: 3, title: 'Post 3', body: 'jfhiuhtucho'},
-        { id: 4, title: 'Post 4', body: 'kwjerconut9'},
-      ],
+      posts: [],
+      // posts: [
+      //   { id: 1, title: 'Post 1', body: 'jiarfshviua'},
+      //   { id: 2, title: 'Post 2', body: 'jekrwhvei5u'},
+      //   { id: 3, title: 'Post 3', body: 'jfhiuhtucho'},
+      //   { id: 4, title: 'Post 4', body: 'kwjerconut9'},
+      // ],
     };
   },
+
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(posts => {
+      this.posts = posts.data
+    }) ;
+  }
 };
 </script>
 
