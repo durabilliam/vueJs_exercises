0ef3731db7dac8175b4cbb2da34c4513ae58bffb
William Perry <durabilliam@gmail.com>
2021-08-09 14:11:34 -0230

    setup inital Vue app

---
+++ vue-apps/demo/src/components/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>HELLO</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PostsExample',
+  data(){
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
